6e7c252edccd046a44354b69dd5a4212903887c4
Dao Lam <Dao.Lam@augensoftwaregroup.com>
2018-07-27 14:17:42 +0700

    Add more navigation methods

--- XFCustomControl/XFCustomControl/XFCustomControl/ContentPageBase.cs
+++ XFCustomControl/XFCustomControl/XFCustomControl/ContentPageBase.cs
@@ -50,5 +50,52 @@ namespace XFCustomControl
                 }
             }
         }
+
+        protected void RemovePages(int fromPage, int numOfPages = 0)
+        {
+            numOfPages = numOfPages > 0 ? numOfPages : Navigation.NavigationStack.Count - fromPage;
+            for (var count = 0; count < numOfPages; count++)
+            {
+                if (fromPage < Navigation.NavigationStack.Count)
+                {
+                    Navigation.RemovePage(Navigation.NavigationStack[fromPage]);
+                }
+            }
+        }
+
+        protected void RemovePagesBetween(Type fromPage, Type toPage)
+        {
+            int fromPageIndex = -1;
+            int toPageIndex = -1;
+            for (int i = 0; i < Navigation.NavigationStack.Count; i++)
+            {
+                if (Navigation.NavigationStack[i].GetType() == fromPage)
+                {
+                    fromPageIndex = i;
+                }
+
+                if (Navigation.NavigationStack[i].GetType() == toPage)
+                {
+                    toPageIndex = i;
+                }
+            }
+
+            if (fromPageIndex > -1 && toPageIndex > -1)
+            {
+                RemovePages(fromPageIndex + 1, toPageIndex - fromPageIndex - 1);
+            }
+        }
+
+        protected void GoBackToPage(Type type)
+        {
+            for (int i = Navigation.NavigationStack.Count - 1; i >= 0; i--)
+            {
+                if (Navigation.NavigationStack[i].GetType() == type)
+                {
+                    RemovePages(i + 1);
+                    return;
+                }
+            }
+        }
     }
 }
